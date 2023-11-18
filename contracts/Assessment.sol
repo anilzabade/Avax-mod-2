f1af2404e7281186743c4ae0abb4d0d661bcb324
anilzabade <150576358+anilzabade@users.noreply.github.com>
2023-11-18 17:43:11 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,29 +1,33 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-//import "hardhat/console.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
+    event Deposit(address indexed account, uint256 amount);
+    event Withdraw(address indexed account, uint256 amount);
+    event PurchaseMembership(address indexed account);
+    event MembershipStatus(address indexed account, string status);
 
-    constructor(uint initBalance) payable {
+    mapping(address => bool) public isMember;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner of this account");
+        _;
+    }
+
+    constructor(uint256 initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
+    function deposit(uint256 _amount) public payable onlyOwner {
+        uint256 _previousBalance = balance;
 
         // perform transaction
         balance += _amount;
@@ -32,15 +36,14 @@ contract Assessment {
         assert(balance == _previousBalance + _amount);
 
         // emit the event
-        emit Deposit(_amount);
+        emit Deposit(msg.sender, _amount);
     }
 
     // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
+    function withdraw(uint256 _withdrawAmount) public onlyOwner {
+        uint256 _previousBalance = balance;
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
@@ -55,6 +58,25 @@ contract Assessment {
         assert(balance == (_previousBalance - _withdrawAmount));
 
         // emit the event
-        emit Withdraw(_withdrawAmount);
+        emit Withdraw(msg.sender, _withdrawAmount);
+    }
+
+    function purchaseMembership() public {
+        require(!isMember[msg.sender], "Already a gym member");
+        isMember[msg.sender] = true;
+
+        // emit the event
+        emit PurchaseMembership(msg.sender);
+    }
+
+    function membershipStatus() public view returns (string memory) {
+        string memory status;
+        if (isMember[msg.sender]) {
+            status = "There is an active gym membership";
+        } else {
+            status = "No active gym membership";
+        }
+
+        return status;
     }
 }
